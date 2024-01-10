180e220cb08e27cfbcd92f257fcbd6aaf06ffa8a
Sasbom <saskervg@live.nl>
2024-01-10 13:22:57 +0100

    basic bot

---
+++ Bot/test_bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord import app_commands
+import asyncio
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@tree.command(
+    name = "ping",
+    guild=discord.Object(id=858640120826560512),
+    description = "Check connection to bot."
+)
+async def ping_command(interaction):
+    await interaction.response.send_message("Pong!")
+
+@client.event
+async def on_ready():
+    # When client is ready, register command tree
+    await tree.sync(guild=discord.Object(id=858640120826560512))
+
+client.run(token="MTE5MzU2OTAzNTQ0NzcxMzk0Mw.Guyf72.ax5uzK769mpeY4OZqSgEYnS4ockxqSYxe6K9EA")
