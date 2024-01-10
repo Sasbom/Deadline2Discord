c18a98df681430a062fe2ed1cbc4b2893de1b4a7
Sasbom <saskervg@live.nl>
2024-01-10 22:06:03 +0100

    Added support for posting embeds n stuff. Todo: add functions to register users.

--- Bot/test_bot.py
+++ Bot/test_bot.py
@@ -26,9 +26,21 @@ class MessageCache:
         while self._messages:
             yield self._messages.pop()
 
+    @property
+    def has_messages(self):
+        return bool(self._messages)
+
 
 MESSAGES = MessageCache()
 
+# testing out an embed.
+embed_msg = discord.Embed(title="Deadline bot!", color=discord.Colour.from_str("#f49221"))
+embed_msg.add_field(name=":abcd: Name: ", value="render_name",inline=False)
+embed_msg.add_field(name=":ocean: Pool: ", value="render_pool",inline=False)
+
+MESSAGES.post_message(embed_msg)
+# end embed test
+    
 class RequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
@@ -56,8 +68,11 @@ async def server_task():
     await client.wait_until_ready()
     channel: discord.TextChannel = client.get_channel(1194649493501653173)
     while not client.is_closed():
-        if MESSAGES._messages:
-            await asyncio.wait([channel.send(message) for message in MESSAGES.messages])
+        if MESSAGES.has_messages:
+            await asyncio.wait([asyncio.create_task(channel.send(message)) 
+                                if not isinstance(message,discord.Embed) 
+                                else asyncio.create_task(channel.send(embed=message)) 
+                                for message in MESSAGES.messages])
         await asyncio.sleep(0.01)
 
 class MyClient(discord.Client):
@@ -67,8 +82,6 @@ class MyClient(discord.Client):
 intents = discord.Intents.default()
 client = MyClient(intents=intents)
 tree = app_commands.CommandTree(client)
-
-
 
 @tree.command(
     name = "ping",
@@ -87,5 +100,5 @@ async def on_ready():
 SERVER_THREAD.start()
 
 client.run(token="MTE5MzU2OTAzNTQ0NzcxMzk0Mw.Guyf72.ax5uzK769mpeY4OZqSgEYnS4ockxqSYxe6K9EA")
-
+DEADLINE_CATCHER.shutdown()
 SERVER_THREAD.join()
